49ea459d89a06f990233e16bad706be6a11c0ced
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2026-08-26 05:18:46 -0700

    Update icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			favicon: '/src/assets/dyno.ico',
+			favicon: '/src/assets/dyno.svg',
 			head: [
 				// Add ICO favicon fallback.
 				{
